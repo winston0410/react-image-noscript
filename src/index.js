c15201cbf2067b0126a8a27dc22ae9d402447800
Hugo <>
2020-10-28 16:00:13 +0800

    :construction: Rewrite in render prop pattern

--- src/index.js
+++ src/index.js
@@ -17,3 +17,33 @@ const withNoScript = (Component) => ({ ...attr }) => {
 }
 
 export default withNoScript
+
+// const Img = withNoScript(
+//   _Img
+// )
+//
+// // Render prop pattern
+//
+// const NoScript = ({ children, ...attr }) => {
+//   const srcPath = defaultWhenEmpty(attr.src)(attr.dataSrc)
+//   const modifiedAttr = {
+//   ...attr, src: srcPath
+//   }
+//
+//   return (
+//     <React.Fragment>
+//       {children(attr)}
+//       <noscript>
+//       {children(modifiedAttr)}
+//       </noscript>
+//     </React.Fragment>
+//   )
+// }
+//
+// export default NoScript
+//
+// const Img = ({ ...attr }) => (
+//   <NoScript>
+//     {(attr) => <_Img {...attr}/>}
+//   </NoScript>
+// )
